945a6821d3e07526e5aaadaea30a4f364abb3692
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-05-10 11:07:10 +0900

    feat(band.hpp): バンドインターフェースを作るだけ作る

---
+++ module/band.hpp
@@ -0,0 +1,15 @@
+/*!
+ * @file band.hpp
+ * @brief 粒子が存在する? バンドについての情報
+ * @author ButterPeanuts
+ * @data 2023-05-10
+*/
+#pragma once
+
+/*!
+ * @brief 粒子が存在する? バンドについての情報のクラス
+ * @details 粒子が存在する? 従う? バンドについての情報のクラス
+ * バンドが関連する物質定数, 定関数の類を表としてまとめる
+*/
+class band{
+};
