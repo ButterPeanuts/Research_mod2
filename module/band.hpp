a478212f2d7dd0d03484965acdfb696c2d6f4f90
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-05-10 11:50:28 +0900

    feat(band.hpp, modeenum.hpp): 列挙子は色んなところから参照するはずなので分離
    #3

--- module/band.hpp
+++ module/band.hpp
@@ -5,18 +5,7 @@
  * @data 2023-05-10
 */
 #pragma once
-
-/*!
- * @enum wave_direction
- * @brief 縦波か横波か
-*/
-enum wave_direction{longitudinal, transverse};
-
-/*!
- * @enum wave_mode
- * @brief 音響モードか光学モードか
-*/
-enum wave_mode{acoustic, optical};
+#include "modeenum.hpp"
 
 /*!
  * @brief 粒子が存在する? バンドについての情報のクラス
