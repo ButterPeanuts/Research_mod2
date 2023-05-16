4e972bea76955ae6c14ef0c60a0a8d19646371e6
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-05-16 21:15:22 +0900

    feat(CMakeLists.txt): curveをlibraryとして追加

--- module/curve.cpp
+++ module/curve.cpp
@@ -8,13 +8,13 @@
 
 curve::curve(){
 	this->table = std::vector<std::pair<double, double>>();
-	auto logger = spdlog::basic_logger_mt("logger", "log/log.log");
-	logger -> set_level(spdlog::level::debug);
-	std::cout << "Hello, World" << std::endl;
-	std::cout << "BUILD_TYPE=" BUILD_TYPE << std::endl;
-	spdlog::info("Hello, World");
-	logger->info("Hello, World");
-	logger->debug("That is japanse true KUSA movie.");
+	/* auto logger = spdlog::basic_logger_mt("logger", "log/log.log"); */
+	/* logger -> set_level(spdlog::level::debug); */
+	/* std::cout << "Hello, World" << std::endl; */
+	/* std::cout << "BUILD_TYPE=" BUILD_TYPE << std::endl; */
+	/* spdlog::info("Hello, World"); */
+	/* logger->info("Hello, World"); */
+	/* logger->debug("That is japanse true KUSA movie."); */
 }
 
 curve::curve(std::string filename){
