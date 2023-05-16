7e7e90e7fa81e2ce02e208213bc972a72e10460c
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-05-17 02:09:30 +0900

    feat(ロガークラスのヘッダーを書いた):

--- module/logger.hpp
+++ module/logger.hpp
@@ -13,8 +13,38 @@
  * 依存性の吸収ぐらいならできると期待してる
 */
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <string>
 namespace mc_sim{
 	class logger{
 		private:
+			/*! spdlogのロガー このクラスが直接操作するところ */
+			spdlog::shared_ptr<spdlog::logger> logger_interface;
+			
+			/*! spdlogのシンク ロガーからロガーを作るときに使う */
+			spdlog::shared_ptr<spdlog::sinks::basic_file_sink_mt> logger_sink;
+		
+		public:
+			/*!
+			 * @brief コンストラクタ
+			 * @param logger_name ロガーの表示名
+			 * @param file_name ロガーが書き込むファイル
+			*/
+			logger(std::string logger_name, std::string file_name);
+			
+			/*!
+			 * @brief ロガーのコピー
+			 * @details コピーというほどではないが, 同じシンクを持つロガーを作るためのメンバ
+			 * @param logger_name ロガーの表示名
+			 * @return mc_sim::logger 同じシンクを持つが, thisとは違うロガー
+			*/
+			mc_sim::logger copy_samesink(std::string logger_name);
+			
+			mc_sim::logger debug(std::string data);
+			mc_sim::logger info(std::string data);
+			mc_sim::logger warn(std::string data);
+			mc_sim::logger error(std::string data);
+			mc_sim::logger critical(std::string data);
 	};
 }
