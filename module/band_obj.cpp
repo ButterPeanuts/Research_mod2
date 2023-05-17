6cf3aa346214cb67b285bf12a8f4592782d7c4b9
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-05-17 13:51:06 +0900

    feat(band_obj.cpp, band_obj.hpp): band_objクラスが完成, curve4つによる情報クラス
    #1, #3

--- module/band_obj.cpp
+++ module/band_obj.cpp
@@ -1,6 +1,33 @@
 #include "modeenum.hpp"
 #include "band_obj.hpp"
+#include <logger.hpp>
+
+band_obj::band_obj(mc_sim::logger& newlogger, curve dos, curve gvelocity, curve domcp, wave_direction direction, wave_mode mode) : dos(dos), gvelocity(gvelocity), domcp(domcp), logger(newlogger){
+	this->directions = direction;
+	this->mode = mode;
+	this->domcp_max = this->domcp.max();
+}
 
 double band_obj::dos_getter(double omega){
-	return 0;
+	return this->dos.itpl_getter(omega);
+}
+
+double band_obj::gvelocity_getter(double omega){
+	return this->gvelocity.itpl_getter(omega);
+}
+
+double band_obj::domcp_getter(double omega){
+	return this->domcp.itpl_getter(omega);
+}
+
+double band_obj::domcp_max_getter(){
+	return this->domcp_max;
+}
+
+wave_direction band_obj::directions_getter(){
+	return this->directions;
+}
+
+wave_mode band_obj::mode_getter(){
+	return this->mode;
 }
--- module/band_obj.hpp
+++ module/band_obj.hpp
@@ -5,8 +5,11 @@
  * @data 2023-05-10
 */
 #pragma once
+#include <string>
 #include "modeenum.hpp"
 #include "band.hpp"
+#include <curve.hpp>
+#include <logger.hpp>
 
 /*!
  * @brief 粒子が存在する? バンドについての情報のクラス
@@ -14,38 +17,46 @@
  * バンドが関連する物質定数, 定関数の類を表としてまとめる
 */
 class band_obj : public band{
-	/*!
-	 * @brief 状態密度(Density of State)を取得する関数
-	 * @param omega 角周波数\f$\omega\f$
-	*/
-	double dos_getter(double omega) override;
-	
-	/*!
-	 * @brief 群速度(Group velocity)を取得する関数
-	 * @param omega 角周波数\f$\omega\f$
-	*/
-	double gvelocity_getter(double omega) override;
-	
-	/*!
-	 * @brief モンテカルロ粒子密度(Density of MonteCalro Particle)を取得する関数
-	 * @details 詳細は久木田(2013)の式2.13を参照
-	 * @param omega 角周波数\f$\omega\f$
-	*/
-	double domcp_getter(double omega) override;
-	
-	/*!
-	 * @brief モンテカルロ粒子密度(Density of MonteCalro Particle)の最大値を取得する関数
-	 * @details かなり純粋なgetter これがないと棄却法にO(n)かかる
-	*/
-	double domcp_max_getter() override;
-	
-	/*!
-	 * @brief 縦波か横波かを取得する関数
-	*/
-	wave_direction directions_getter() override;
-	
-	/*!
-	 * @brief 音響波か光学波か取得する関数
-	*/
-	wave_mode mode_getter() override;
+	private:
+		curve dos, gvelocity, domcp;
+		double domcp_max;
+		wave_direction directions;
+		wave_mode mode;
+		mc_sim::logger& logger;
+	public:
+		band_obj(mc_sim::logger& newlogger, curve dos, curve gvelocity, curve domcp, wave_direction direction, wave_mode mode);
+		/*!
+		 * @brief 状態密度(Density of State)を取得する関数
+		 * @param omega 角周波数\f$\omega\f$
+		*/
+		double dos_getter(double omega) override;
+		
+		/*!
+		 * @brief 群速度(Group velocity)を取得する関数
+		 * @param omega 角周波数\f$\omega\f$
+		*/
+		double gvelocity_getter(double omega) override;
+		
+		/*!
+		 * @brief モンテカルロ粒子密度(Density of MonteCalro Particle)を取得する関数
+		 * @details 詳細は久木田(2013)の式2.13を参照
+		 * @param omega 角周波数\f$\omega\f$
+		*/
+		double domcp_getter(double omega) override;
+		
+		/*!
+		 * @brief モンテカルロ粒子密度(Density of MonteCalro Particle)の最大値を取得する関数
+		 * @details かなり純粋なgetter これがないと棄却法にO(n)かかる
+		*/
+		double domcp_max_getter() override;
+		
+		/*!
+		 * @brief 縦波か横波かを取得する関数
+		*/
+		wave_direction directions_getter() override;
+		
+		/*!
+		 * @brief 音響波か光学波か取得する関数
+		*/
+		wave_mode mode_getter() override;
 };
