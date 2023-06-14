24d806fe65bdbfb6dacaa75af3f3b37560db6938
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-06-14 10:33:33 +0900

    fix(simulation): simulation::heatcap_curve_constructのloggerを参照で取るように
    #36

--- module/massconst.hpp
+++ module/massconst.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include<functional>
 #include<algorithm>
+#include<memory>
 
 #include<curve.hpp>
 #include<band.hpp>
@@ -33,7 +34,7 @@ class massconst {
 		
 		//比熱テーブルを構築する
 		//static void Si_heatcap_table_construct();
-		static curve heatcap_curve_construct(std::vector<std::shared_ptr<band>>, mc_sim::logger);
+		static curve heatcap_curve_construct(std::vector<std::shared_ptr<band>>, mc_sim::logger&);
 		
 		/* static double Si_group_velocity(double angular_frequency, int bandnum); */
 };
