428974c5b627607a1cdc299f6ff2d5306a8dffeb
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-07-19 10:46:14 +0900

    feat(scatconst): 逆緩和時間を出すメソッドの定義を書いた
    #60

--- module/scatconst.hpp
+++ module/scatconst.hpp
@@ -40,4 +40,21 @@ class scatconst{
 	double xi();
 	double c();
 	double f();
+	
+	/*!
+	 * @brief フォノン-フォノン散乱の緩和時間
+	 * @param double 角周波数\f$\omega\f$
+	 * @param double 温度\f$T\f$
+	*/
+	double tau_u_inv(double, double);
+	/*!
+	 * @brief フォノン-欠陥散乱の緩和時間
+	 * @param double 角周波数\f$\omega\f$
+	*/
+	double tau_d_inv(double);
+	/*!
+	 * @brief フォノン-境界散乱の緩和時間
+	 * @param double 群速度\f$\bar(v)\f$
+	*/
+	double tau_b_inv(double);
 };
